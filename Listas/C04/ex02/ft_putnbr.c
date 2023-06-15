53be8049b055645e37ed415585b9de22a75add52
Veronica Lima De Faria <vlima-de@c3r4p1.42sp.org.br>
2023-06-15 12:42:36 -0300

    piscina

---
+++ Listas/C04/ex02/ft_putnbr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlima-de <vlima-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 14:48:08 by vlima-de          #+#    #+#             */
+/*   Updated: 2023/06/14 14:59:02 by vlima-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(42);
+}
